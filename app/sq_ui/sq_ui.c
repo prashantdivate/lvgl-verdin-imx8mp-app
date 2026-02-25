cdf63bc03f429e5773c1f2004a3849c9f089d4ed
Prashant Divate <diwateprashant44@gmail.com>
2026-02-25 05:36:07 -0500

    This will bring the MAD logo animation direction back to what SquareLine exported.

--- app/sq_ui/sq_ui.c
+++ app/sq_ui/sq_ui.c
@@ -35,7 +35,7 @@ lv_anim_t * upanim_Animation(lv_obj_t * TargetObject, int delay)
     lv_anim_set_time(&PropertyAnimation_0, 700);
     lv_anim_set_user_data(&PropertyAnimation_0, PropertyAnimation_0_user_data);
     lv_anim_set_custom_exec_cb(&PropertyAnimation_0, _ui_anim_callback_set_y);
-    lv_anim_set_values(&PropertyAnimation_0, 20, 0);
+    lv_anim_set_values(&PropertyAnimation_0, -200, 0);
     lv_anim_set_path_cb(&PropertyAnimation_0, lv_anim_path_ease_out);
     lv_anim_set_delay(&PropertyAnimation_0, delay + 0);
     lv_anim_set_deleted_cb(&PropertyAnimation_0, _ui_anim_callback_free_user_data);
